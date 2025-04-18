b77b036c029a94c30a08559d5a5b7299d84771dd
HarlowMoran <hmoran72@smith.edu>
2025-04-18 11:08:22 -0400

    Main and Game

---
+++ Game.java
@@ -0,0 +1,33 @@
+import java.awt.Image;
+import java.util.ArrayList;
+public class Game {
+
+    //Attributes
+    private String currentLocation;
+    private Image map;
+    private ArrayList<String> inventory;
+
+    private Game(String currentLocation, Image map, ArrayList<String> inventory){
+      this.currentLocation = currentLocation;
+      this.map = map;
+      this.inventory = inventory;
+
+    }
+
+    
+
+
+    
+    private void Treasure(Boolean isFound){
+        if(isFound == true){
+           // Eva suggested an image of the trasure pop up, saying something along the lines of "you have won the game!"
+        }
+        else{
+           System.out.println("The treasure has yet to be found!");
+        }
+     }
+     
+    
+
+
+}
---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+   public static void main(String[] args) {
+    System.out.println("WELCOME TO WONDERLAND");
+
+    //Insert image of map
+
+
+
+
+
+
+
+
+   }
+}
+
