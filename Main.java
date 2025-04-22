f7ad8224f4ff1701210fe341968d4930aaeaa25d
HarlowMoran <hmoran72@smith.edu>
2025-04-22 10:20:08 -0400

    Edits

--- Main.java
+++ Main.java
@@ -1,19 +1,11 @@
 import java.util.Scanner;
 public class Main {
    public static void main(String[] args) {
-    System.out.println("WELCOME TO WONDERLAND (testing commit)");
+    System.out.println("WELCOME TO WONDERLAND");
 
     //Insert image of map
     MapDisplay.showMap("map.png");
    }
 }
 
-
-
-
-
-
-
-   }
-}
 
