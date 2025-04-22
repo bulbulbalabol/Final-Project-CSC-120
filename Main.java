23cf6368f645811034fab1a8f4dd2e91abfafe99
HarlowMoran <hmoran72@smith.edu>
2025-04-22 10:18:57 -0400

    test commit

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
    public static void main(String[] args) {
-    System.out.println("WELCOME TO WONDERLAND");
+    System.out.println("WELCOME TO WONDERLAND (testing commit)");
 
     //Insert image of map
     MapDisplay.showMap("map.png");
