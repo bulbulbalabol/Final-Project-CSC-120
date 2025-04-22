47ea22f425b17e6620f55b81c1b804262c735cfa
HarlowMoran <hmoran72@smith.edu>
2025-04-22 09:43:57 -0400

    Scanner

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 public class Main {
    public static void main(String[] args) {
     System.out.println("WELCOME TO WONDERLAND");
 
     //Insert image of map
     MapDisplay.showMap("map.png");
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("Where would you like to go?");
+    String response = input.nextLine();
+
+
    }
 }
 
