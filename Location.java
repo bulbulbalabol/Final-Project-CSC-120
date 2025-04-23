b78a972bce87ad286694ee067f0700997d9f17e0
Evasobo <esoboleva@smith.edu>
2025-04-22 20:58:50 -0400

    j

--- Location.java
+++ Location.java
@@ -1,3 +1,35 @@
+import java.util.List;
+
 public class Location {
-    
+    private String name;
+    private String description;
+    private List<String> choices;
+    private List<String> outcomes;
+
+    public Location(String name, String description, List<String> choices, List<String> outcomes) {
+        this.name = name;
+        this.description = description;
+        this.choices = choices;
+        this.outcomes = outcomes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<String> getChoices() {
+        return choices;
+    }
+
+    public String getOutcome(int index) {
+        if (index >= 0 && index < outcomes.size()) {
+            return outcomes.get(index);
+        } else {
+            return "Invalid choice.";
+        }
+    }
 }
