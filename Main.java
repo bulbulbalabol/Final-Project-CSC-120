104ee41e27bee4031cfa0262e97a16ce90ce4439
HarlowMoran <hmoran72@smith.edu>
2025-04-23 15:31:23 -0400

    Edit

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
 public class Main {
    public static void main(String[] args) {
-    System.out.println("WELCOME TO WONDERLAND");
-
-    //Insert image of map
-    MapDisplay.showMap("map.png");
 
     GameEngine engine = new GameEngine();
         engine.startGame();
