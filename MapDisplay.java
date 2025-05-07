7dd820f364edc7e3ab976f568985cc92b2f42102
HarlowMoran <hmoran72@smith.edu>
2025-05-07 14:23:34 -0400

    edit

--- MapDisplay.java
+++ MapDisplay.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 /**
- * The {@code MapDisplay} class provides a utility method to display
+ * The MapDisplay class provides a utility method to display
  * an image of a map in a graphical window using Java Swing.
  */
 
@@ -9,7 +9,7 @@ public class MapDisplay {
     /**
      * Displays a map image in a GUI window with the title "Treasure Map".
      * <p>
-     * This method creates a new {@code JFrame}, loads an image from the specified
+     * This method creates a new JFrame, loads an image from the specified
      * path, and shows it centered on the screen.
      *
      * @param imagePath the file path to the map image to be displayed
